cfac2b5b5a83b6f56448e1f4776452667f22bcc8
prayag <prayagd21@gmail.com>
2020-03-27 22:20:17 +0530

    routing, express, npm

--- app.js
+++ app.js
@@ -3,13 +3,29 @@ let fs = require('fs');
 
 let server = http.createServer((req, res) => {
 	console.log('request was made: ' + req.url);
-	res.writeHead(200, { 'Content-Type': 'application/json' });
-	let myObj = {
-		name: 'ryu',
-		job: 'ninja',
-		age: 21
-	};
-	res.end(JSON.stringify(myObj));
+	if (req.url === '/home' || req.url === '/') {
+		res.writeHead(200, { 'Content-Type': 'text/html' });
+		fs.createReadStream(__dirname + '/index.html').pipe(res);
+	} else if (req.url === '/contact') {
+		res.writeHead(200, { 'Content-Type': 'text/html' });
+		fs.createReadStream(__dirname + '/contact.html').pipe(res);
+	} else if (req.url === '/api/ninjas') {
+		let myObj = [
+			{
+				name: 'ryu',
+				age: 21
+			},
+			{
+				name: 'moshi',
+				age: 22
+			}
+		];
+		res.writeHead(200, { 'Content-Type': 'application/json' });
+		res.end(JSON.stringify(myObj));
+	} else {
+		res.writeHead(200, { 'Content-Type': 'text/html' });
+		fs.createReadStream(__dirname + '/404.html').pipe(res);
+	}
 });
 
 server.listen(3000, '127.0.0.1');
