a2570c4b84959b0d36d61493c402fd7d6c5b0106
prayag <prayagd21@gmail.com>
2020-04-01 22:00:51 +0530

    todo app

---
+++ todo/controllers/todoController.js
@@ -0,0 +1,9 @@
+module.exports = function(app) {
+	app.get('/todo', (req, res) => {
+		res.render('todo');
+	});
+
+	app.post('/todo', (req, res) => {});
+
+	app.delete('/todo', (req, res) => {});
+};
