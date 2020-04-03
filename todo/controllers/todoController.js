84a1d5f8db0769833bac6efec2e1e4ffd33f988b
prayag <prayagd21@gmail.com>
2020-04-03 22:31:59 +0530

    nosql database, mongoose

--- todo/controllers/todoController.js
+++ todo/controllers/todoController.js
@@ -1,9 +1,17 @@
+let bodyParser = require('body-parser');
+
+let data = [ { item: 'get milk' }, { item: 'walk dog' }, { item: 'kick some coding ass' } ];
+let urlencodedParser = bodyParser.urlencoded({ extended: false });
+
 module.exports = function(app) {
 	app.get('/todo', (req, res) => {
-		res.render('todo');
+		res.render('todo', { todos: data });
 	});
 
-	app.post('/todo', (req, res) => {});
+	app.post('/todo', urlencodedParser, (req, res) => {
+		data.push(req.body);
+		res.json(data);
+	});
 
 	app.delete('/todo', (req, res) => {});
 };
